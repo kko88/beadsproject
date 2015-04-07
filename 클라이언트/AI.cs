761e83500a1e1cf66d03171c59509d7ca40aed2e
kko88 <mymy_kko@naver.com>
2015-04-07 18:47:31 +0900

    몹 AI
    몹 AI

---
+++ 클라이언트/AI.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(MobMove))]
+[RequireComponent(typeof(SphereCollider))]
+public class AI : MonoBehaviour {
+
+    public float preceptionRadius = 10;
+     public float baseMeleeRange = 4;
+     public Transform target;
+     
+     private Transform _myTransform;
+     private const float ROTATION_DAMP = .03f;
+     private const float FORWARD_DAMP = 0.9f;
+
+    void Start()
+    {
+        SphereCollider sc = GetComponent<SphereCollider>();
+        CharacterController cc = GetComponent<CharacterController>();
+
+        if (sc == null){
+            Debug.LogError("이 몹에 SphereCollider 없음");
+        }
+        else
+        {
+            sc.isTrigger = true;
+        }
+
+        if (cc == null)
+        {
+            Debug.LogError("이 몹에 컨트롤러 없음");
+        }
+
+        else
+        {
+            sc.center = cc.center;
+            sc.radius = preceptionRadius;
+        }
+
+        _myTransform = transform;
+
+ //       GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+ //       if (go == null)
+ //           Debug.Log("몹이 플레이어 검색못함");
+
+ //       target = go.transform;
+    }
+
+ 
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 dir = (target.position - _myTransform.position).normalized;
+            float direction = Vector3.Dot(dir, transform.forward);
+
+
+            float dist = Vector3.Distance(target.position, _myTransform.position);
+
+           // Debug.Log(dist);
+
+            
+            if (direction > FORWARD_DAMP && dist > baseMeleeRange)
+            {
+                SendMessage("MoveMeForward", MobMove.Forward.forward);
+            }
+            else
+            {
+                SendMessage("MoveMeForward", MobMove.Forward.none);
+            }
+            
+
+
+            dir = (target.position - _myTransform.position).normalized;
+            direction = Vector3.Dot(dir, transform.right);
+
+            if (direction > ROTATION_DAMP)
+            {
+                SendMessage("RotateMe", MobMove.Turn.right);
+            }
+            else if (direction < ROTATION_DAMP)
+            {
+                SendMessage("RotateMe", MobMove.Turn.left);
+            }
+            else
+            {
+                SendMessage("RotateMe", MobMove.Turn.none);
+            }
+
+        }
+        else
+        {
+            SendMessage("MoveMeForward", MobMove.Forward.none);
+            SendMessage("RotateMe", MobMove.Turn.none);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+    //    Debug.Log("이벤트");
+        if (other.CompareTag("Player"))
+        {
+            target = other.transform;
+        }
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+    //    Debug.Log("이벤트아님");
+        if (other.CompareTag("Player"))
+        {
+            target = null;
+        }
+    }
+  
+}
---
+++ 클라이언트/MobMove.cs
@@ -0,0 +1,221 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 몬스터 무브 AI
+/// </summary>
+
+[RequireComponent(typeof(CharacterController))]
+public class MobMove : MonoBehaviour
+{
+
+
+    public enum State
+    {
+        Idle,
+        Init,
+        Setup,
+        Run
+    }
+    public enum Turn
+    {
+        left = -1,
+        none = 0,
+        right = 1
+    }
+
+    public enum Forward
+    {
+        back = -1,
+        none = 0,
+        forward = 1
+    }
+
+    public float walkSpeed = 10;
+    public float runMultiplier = 2;   // 달리기 배수
+    public float strafeSpeed = 8.0f;  // 횡이동
+    public float rotateSpeed = 250;
+    public float gravity = 20;
+    public float airTime = 0;
+    public float fallTime = 0.5f;
+    public float jumpHeight = 10;  // 점프높이
+    public float jumpTime = 1.5f;
+
+    public CollisionFlags _collisionFlags;
+    private Vector3 _moveDirection;
+    private Transform _myTransform;
+    private CharacterController _controller;
+
+    private Turn _turn;
+    private Forward _forward;
+    private Turn _strafe;
+    private bool _run;
+    private bool _jump;
+
+    private State _state;
+
+    public void Awake()
+    {
+        _myTransform = transform;
+        _controller = GetComponent<CharacterController>();
+        _state = MobMove.State.Init;
+    }
+
+    IEnumerator Start()
+    {
+
+        while (true)
+        {
+            switch (_state)
+            {
+                case State.Init:
+                    Init();
+                    break;
+                case State.Setup:
+                    SetUp();
+                    break;
+                case State.Run:
+                    ActionPicker();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    private void Init()
+    {
+        if (!GetComponent<CharacterController>()) return;
+        if (!GetComponent<Animation>()) return;
+
+        _state = MobMove.State.Setup;
+    }
+    private void SetUp()
+    {
+        _moveDirection = Vector3.zero;
+        animation.Stop();
+        animation.wrapMode = WrapMode.Loop;
+  //      animation["jump"].layer = -1;
+  //      animation["jump"].wrapMode = WrapMode.Once; // 점프 한번만
+        animation.Play("Idle");
+        _turn = MobMove.Turn.none;
+        _forward = MobMove.Forward.none;
+        _strafe = Turn.none;
+        _run = true;        // 토글런 설정 On/Off
+        _jump = false;
+
+        _state = MobMove.State.Run;
+    }
+    private void ActionPicker()
+    {
+        _myTransform.Rotate(0, (int)_turn * Time.deltaTime * rotateSpeed, 0);
+
+
+        if (_controller.isGrounded)
+        {
+            airTime = 0;
+            _moveDirection = new Vector3((int)_strafe, 0, (int)_forward);
+            _moveDirection = _myTransform.TransformDirection(_moveDirection).normalized;
+            _moveDirection *= walkSpeed;
+
+            if (_forward != Forward.none)
+            {
+                if (_run)
+                {
+                    _moveDirection *= runMultiplier;
+                    Run();
+                }
+                else
+                {
+                    Walk();
+                }
+            }
+            else if (_strafe != MobMove.Turn.none)
+            {
+                Strafe();
+            }
+            else
+            {
+                Idle();
+            }
+
+            if (_jump)
+            {
+                if (airTime < jumpTime)
+                {
+                    _moveDirection.y += jumpHeight;
+                    Jump();
+                    _jump = false;
+                }
+
+            }
+        }
+        else
+        {
+            if ((_collisionFlags & CollisionFlags.CollidedBelow) == 0)
+            {
+                airTime += Time.deltaTime;
+                if (airTime > fallTime)
+                {
+                    Fall();
+                }
+            }
+
+        }
+
+        _moveDirection.y -= gravity * Time.deltaTime;
+        _collisionFlags = _controller.Move(_moveDirection * Time.deltaTime);
+    }
+
+
+    public void MoveMeForward(Forward z)
+    {
+        _forward = z;
+    }
+
+    public void ToggleRun()
+    {
+        _run = !_run;  //   토글설정
+    }
+
+    public void RotateMe(Turn y)
+    {
+        _turn = y;
+    }
+
+    public void Strafe(Turn x)
+    {
+        _strafe = x;
+    }
+
+    public void JumpUp()
+    {
+        _jump = true;
+    }
+    public void Idle()
+    {
+        animation.CrossFade("Idle");
+    }
+
+    public void Walk()
+    {
+        animation.CrossFade("balance_walk");
+    }
+    public void Run()
+    {
+        animation["Run"].speed = 1.5f;
+        animation.CrossFade("Run");
+    }
+    public void Jump()
+    {
+        animation.CrossFade("jump");
+    }
+    public void Strafe()
+    {
+        animation.CrossFade("roll_side");
+    }
+
+    public void Fall()
+    {
+        animation.CrossFade("fall");
+    }
+
+}
